e58096d3131756ade3e322fed05c5425431c16c1
diao jun wen <diao jun wen>
2018-09-18 20:59:00 +0800

    'updata'

--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -1,3 +1,5 @@
 /*
 包含n个action type名称常量的模块
  */
+export const AUTH_SUCCESS = 'AUTH_SUCCESS'
+export const ERROR_MSG = 'ERROR_MSG'
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,3 +3,37 @@
 同步action: 对象
 异步action: dispatch函数
  */
+ import {AUTH_SUCCESS,ERROR_MSG} from "./action-types";
+ import {reqRegister,reqLogin} from "../api/index";
+ //同步错误消息
+const errorMsg = (msg) =>({type:ERROR_MSG,data:msg})
+//同步成功响应
+const authSuccess = (user)=>({type:AUTH_SUCCESS,data:user})
+//注册的异步
+export function register() {
+  return dispatch=>{//执行异步的代码,发异步ajax请求
+    reqRegister({username,password,type}).then(response=>{
+      const result = response.data
+      if(result.code==0){
+        dispatch(authSuccess(user))
+      }else{
+        const msg = result.msg
+        dispatch(errorMsg(msg))
+      }
+    })
+  }
+}
+//登录的异步
+export function login() {
+  return dispatch=>{//执行异步的代码,发异步ajax请求
+    reqLogin({username,password}).then(response=>{
+      const result = response.data
+      if(result.code==0){
+        dispatch(authSuccess(user))
+      }else{
+        const msg = result.msg
+        dispatch(errorMsg(msg))
+      }
+    })
+  }
+}
