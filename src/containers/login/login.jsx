73a059c532a99bda935afc5a311996f842ce9d3e
diao jun wen <diao jun wen>
2018-09-18 23:36:55 +0800

    'updata'

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,3 +1,75 @@
+/*
+import React, {Component} from 'react'
+import {NavBar, WingBlank, List, WhiteSpace, InputItem, Button} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+
+import Logo from '../../componnets/logo/logo'
+import {login} from '../../redux/actions'
+
+/!*
+登陆路由组件
+ *!/
+export default class Login extends Component {
+
+  state = {
+    username: '',
+    password: '',
+  }
+
+  // 处理输入发生改变的监听回调
+  handleChange = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
+  // 登陆的回调
+  login = () => {
+    //this.props.login(this.state)
+  }
+
+  goRegister = () => {
+    // 编程式路由导航(跳转)
+    this.props.history.replace('/register')
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            <InputItem
+              placeholder='请输入用户名'
+              onChange={val => this.handleChange('username', val)}
+            >
+              用户名:
+            </InputItem>
+            <WhiteSpace/>
+
+            <InputItem
+              type='password'
+              placeholder='请输入密码'
+              onChange={val => this.handleChange('password', val)}
+            >
+              密码:
+            </InputItem>
+            <WhiteSpace/>
+
+            <Button type='primary' onClick={this.login}>登&nbsp;&nbsp;陆</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goRegister}>还没有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
+    )
+  }
+}*/
+
+
+
 import React, {Component} from 'react'
 import {NavBar, WingBlank, List, WhiteSpace, InputItem, Button} from 'antd-mobile'
 
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,16 +1,20 @@
+/*
 import React, {Component} from 'react'
 import {NavBar, WingBlank, List, WhiteSpace, InputItem, Radio, Button} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
 
 import Logo from '../../componnets/logo/logo'
-
 import {reqRegister} from '../../api'
+import {register} from "../../redux/actions";
+import '../../componnets/error/error.less'
 
 const ListItem = List.Item
 
-/*
+/!*
 注册路由组件
- */
-export default class Register extends Component {
+ *!/
+class Register extends Component {
 
   state = {
     username: '',
@@ -28,11 +32,12 @@ export default class Register extends Component {
 
   // 注册的回调
   register = () => {
-    console.log(this.state)
+    // console.log(this.state)
+    //
+    // reqRegister(this.state).then(response => {
+    //   console.log(response.data) // {code:0, data: user}  {code:1, message: "此用户已用户"}
+    this.props.register(this.state)
 
-    reqRegister(this.state).then(response => {
-      console.log(response.data) // {code:0, data: user}  {code:1, message: "此用户已用户"}
-    })
   }
 
   goLogin = () => {
@@ -42,11 +47,16 @@ export default class Register extends Component {
 
   render () {
     const {type} = this.state
+    const {redirectTo,msg} = this.props
+    if(redirectTo){
+      return <Redirect to={redirectTo}/>//注册成功直接跳转到页面
+    }
     return (
       <div>
         <NavBar>硅谷直聘</NavBar>
         <Logo/>
         <WingBlank>
+          {msg ? <p className='error-msg'>{msg}</p>:null}
           <List>
             <InputItem
               placeholder='请输入用户名'
@@ -80,3 +90,102 @@ export default class Register extends Component {
     )
   }
 }
+export default connect(
+  state => ({user:state.user}),
+  {register}
+)(Register)*/
+
+
+import React, {Component} from 'react'
+import {NavBar, WingBlank, List, WhiteSpace, InputItem, Radio, Button} from 'antd-mobile'
+import {Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+import {register} from '../../redux/actions'
+import Logo from '../../componnets/logo/logo'
+
+const ListItem = List.Item
+
+/*
+注册路由组件
+ */
+class Register extends Component {
+
+  state = {
+    username: '',
+    password: '',
+    password2: '',
+    type: 'dashen'  // dashen/laoban
+  }
+
+  // 处理输入发生改变的监听回调
+  handleChange = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
+  // 注册的回调
+  register = () => {
+    console.log(this.state)
+    this.props.register(this.state)
+  }
+
+  goLogin = () => {
+    // 编程式路由导航(跳转)
+    this.props.history.replace('/login')
+  }
+
+  render () {
+    const {type} = this.state
+    const {msg, redirectTo} = this.props.user
+    // 如果redirectTo有值, 就需要自动跳转到对应的路径
+    if(redirectTo) {
+      // render函数中需要自动跳转
+      return <Redirect to={redirectTo}></Redirect>
+    }
+
+    return (
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            {msg ? <p className='error-msg'>{msg}</p> : null}
+            <InputItem
+              placeholder='请输入用户名'
+              onChange={val=> this.handleChange('username', val)}
+            >
+              用户名:
+            </InputItem>
+            <WhiteSpace/>
+            <InputItem
+              type='password'
+              placeholder='请输入密码'
+              onChange={val=> this.handleChange('password', val)}
+            >
+              密码:
+            </InputItem>
+            <WhiteSpace/>
+            <InputItem type='password' placeholder='请输入确认密码' onChange={val=> this.handleChange('password2', val)}>确认密码: </InputItem>
+            <WhiteSpace/>
+            <List.Item>
+              <span>用户类型: </span>&nbsp;&nbsp;&nbsp;
+              <Radio checked={type==='laoban'} onChange={() => this.handleChange('type', 'laoban')}>老板</Radio>&nbsp;&nbsp;&nbsp;
+              <Radio checked={type==='dashen'} onChange={() => this.handleChange('type', 'dashen')}>大神</Radio>
+            </List.Item>
+            <WhiteSpace/>
+            <Button type='primary' onClick={this.register}>注&nbsp;&nbsp;册</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goLogin}>已有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  state => ({user: state.user}),  // {user: user()}
+  {register}
+)(Register)
